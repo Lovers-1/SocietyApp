14c07515388c88bd75d076b6fe88dd5d21c7a908
Thapelo-git <85837015+Thapelo-git@users.noreply.github.com>
2021-11-22 10:40:34 +0200

    thapelo

---
+++ components/thapelo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View,Text} from 'react-native'
+
+
+const Thapelo = ()=>{
+    return(
+        <View>
+            <Text>Thapelo</Text>
+        </View>
+    )
+}
+export default Thapelo
